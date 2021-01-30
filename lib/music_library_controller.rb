0acef75be4b35c8c334d0363de733564105babb9
Fay Vera <fvera.cavalcante@gmail.com>
2021-01-30 14:51:01 -0500

    commit tree 912b01504bd6433c3a2e546b025a18caa62f5241

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -42,7 +42,7 @@ class MusicLibraryController
         end
         def list_artists
             # binding.pry
-            Artist.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |a, i|
+            Artist.all.uniq.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |a, i|
               puts "#{i}. #{a.name}"
             end
           end
